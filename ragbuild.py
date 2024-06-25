de18eb0cd5b33c2bd16b49a25195a2d6e4243cbc
shubhammanuwas <shubham.manuwas2@gmail.com>
2024-06-25 10:40:36 +0530

    using llama parse

--- ragbuild.py
+++ ragbuild.py
@@ -5,6 +5,7 @@ from llama_index.llms.openai import OpenAI
 from llama_index.core.tools import FunctionTool
 from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, Settings
 from llama_index.core.tools import QueryEngineTool
+from llama_parse import LlamaParse
 
 Settings.llm = OpenAI(model="gpt-3.5-turbo", temperature=0)
 
@@ -25,10 +26,31 @@ documents = SimpleDirectoryReader("./data").load_data()
 index = VectorStoreIndex.from_documents(documents)
 query_engine = index.as_query_engine()
 
-response= query_engine.query(
-    "What is the total amount of the Canadian Federal budget"
+
+#after loading the llama api
+documents2= LlamaParse(result_type="markdown").load_data(
+    "./data/2023_canadian_budget.pdf"
 )
-print(response)
+index2=VectorStoreIndex.from_documents(documents2)
+query_engine2=index2.as_query_engine()
+
+
+
+
+
+# response= query_engine.query(
+#     "What is the total amount of the Canadian Federal budget"
+# )
+# print(response)
+# response = query_engine.query(
+#     "How much exactly was allocated to a tax credit to promote investment in green technologies in the 2023 Canadian federal budget?"
+# )
+# print(response)
+
+response2=query_engine2.query(
+        "How much exactly was allocated to a tax credit to promote investment in green technologies in the 2023 Canadian federal budget?"
+)
+print(response2)
 
 budget_tool=QueryEngineTool.from_defaults(
     query_engine,

6a37d39f6a853deef9de76ab178fdf00a6f449d4
shubhammanuwas <shubham.manuwas2@gmail.com>
2024-06-25 10:13:24 +0530

    rag intro

--- ragbuild.py
+++ ragbuild.py
@@ -8,11 +8,38 @@ from llama_index.core.tools import QueryEngineTool
 
 Settings.llm = OpenAI(model="gpt-3.5-turbo", temperature=0)
 
+#multiply tool and add tool
+def multiply(a: float, b: float) -> float:
+    """Multiply two numbers and returns the product"""
+    return a * b
+multiply_tool = FunctionTool.from_defaults(fn=multiply)
+
+def add(a: float, b: float) -> float:
+    """Add two numbers and returns the sum"""
+    return a + b
+add_tool = FunctionTool.from_defaults(fn=add)
+
+
+
 documents = SimpleDirectoryReader("./data").load_data()
 index = VectorStoreIndex.from_documents(documents)
 query_engine = index.as_query_engine()
 
 response= query_engine.query(
     "What is the total amount of the Canadian Federal budget"
+)
+print(response)
+
+budget_tool=QueryEngineTool.from_defaults(
+    query_engine,
+    name="canadian_budget_2023",
+    description="A RAG engine being built in Shubham's Macbook which contains some basic facts and info about Canadian Budget 2023"
+)
+agent= ReActAgent.from_tools(
+    [multiply_tool, add_tool, budget_tool], verbose=True
+)
+
+response=agent.chat(
+    "What is the total amount of the 2023 Canadian federal budget multiplied by 3? Go step by step, using a tool to do any math."
 )
 print(response)
